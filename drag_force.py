a0b2c7870ab30567b6f5a1166a88b4e3b356fa7d
dborders643 <dborders@anderson.edu>
2026-03-17 17:38:04 -0400

    feat: drag animation

--- drag_force.py
+++ drag_force.py
@@ -3,7 +3,7 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 
 # ----------Setup---------- 
-v0 = 10.0                           # launch speed [m/s]
+v0 = 100.0                          # launch speed [m/s]
 phi_deg = 45.0                      # launch angle [deg]
 phi = np.deg2rad(phi_deg)           # launch angle [rad]
 vx0 = v0*np.cos(phi)                # launch speed (horizontal component) [m/s] 
@@ -13,8 +13,8 @@ x0 = 0.0                            # launch position (horizontal component) [m]
 y0 = 0.0                            # launch position (vertical component) [m]
 g = 9.81                            # gravational constant [m/s^2]
 b = 0.1                             # drag coefficient [units don't matter]
-tf = 3 * (2 * vy0/g)                # final time [s] (overshoot on purpose)
-t = np.linspace(t0, tf, num=100)    # time array [s]
+tf = 100                            # final time [s] (overshoot on purpose)
+t = np.linspace(t0, tf, num=10000)  # time array [s]
 dt = t[1] - t[0]                    # scalar time step [s]
 
 # Preallocation
@@ -26,37 +26,35 @@ dvxdt = np.zeros_like(t)
 dvydt = np.zeros_like(t)
 vx = np.zeros_like(t)
 vy = np.zeros_like(t)
+v = np.zeros_like(t)
 
 # Initial Conditions
 x[0] = x0
 y[0] = y0
 vx[0] = vx0
 vy[0] = vy0
+v[0] = v0
 
 # ----------Logic----------
 for i in range(len(t) - 1):
-    v = sqrt(vx**2 = vy**2)
     # Derivatives
     dxdt[i] = vx[i]
     dydt[i] = vy[i]
-    dvxdt[i] = -b*v[i]**2*np.cos(phi)
-    dvydt[i] = 
+    v[i] = np.sqrt(vx[i]**2 + vy[i]**2)
+    dvxdt[i] = -b*v[i]*vx[i]
+    dvydt[i] = -g - b*v[i]*vy[i]
     # Ground Condition
-    if y <= 0 and i > 0:
+    if y[i] <= 0 and i > 0:
         break
     # Update
-    dxdt[i+1] = dxdt[i] + dvxdt[i]*dt
-    dydt[i+1] = dydt[i] + dvydt[i]*dt
+    vx[i+1] = vx[i] + dvxdt[i]*dt
+    vy[i+1] = vy[i] + dvydt[i]*dt
     x[i+1] = x[i] + dxdt[i]*dt
     y[i+1] = y[i] + dydt[i]*dt
     t[i+1] = t[i] + dt
-# ----------Visual---------- 
-# Motion graph
-plt.plot(x, y)
-plt.xlabel("Horizontal Distance (m)")
-plt.ylabel("Vertical Distance (m)")
-plt.title("Projectile Motion with Drag")
-plt.show()
+# Trim zeros
+x = x[:i]
+y = y[:i]
 
 # ----------Animation----------
 fig, ax = plt.subplots()
@@ -71,6 +69,9 @@ def update(i):
     ball.set_data([x[i]], [y[i]])
     return ball,
 
-ani = FuncAnimation(fig, update, frames=len(t),
+ani = FuncAnimation(fig, update, frames=len(x),
                     init_func=init, interval=20)
+ax.set_xlabel("Horizontal Distance (m)")
+ax.set_ylabel("Vertical Distance (m)")
+ax.set_title("Projectile Motion with Drag")
 plt.show()
