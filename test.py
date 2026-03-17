16b74abbd0f24ab5883a74886d423ec29be9ca4e
dborders643 <dborders@anderson.edu>
2026-03-17 10:52:47 -0400

    update

--- test.py
+++ test.py
@@ -9,12 +9,24 @@ t = 10.0        # final time [s]
 x0 = 0.0        # launch position (horizontal component) [m]
 y0 = 0.0        # launch position (vertical component) [m]
 
-# Setup
+# Setup 
 dt = np.linspace(t0, t, num=1000)
 x = np.zeros_like(dt)
+# Preallocation
 y = np.zeros_like(dt)
+vx = np.zeros_like(dt)
+vy = np.zeros_like(dt)
+
+vx0 = v0*np.cos(phi)
+vy0 = v0*np.sin(phi)
+
+np.cos()
 
 # Equations
-for _ in range(np.size(dt)):
+for i in range(np.size(dt)):
     
+
+    # Update Equations (Euler Method)
+    x(i+1) = x(i) + dvxdt(i)*dt(i)
+    y(i+1) = y(i) + dvydt(i)*dt(i)
 # Visualize 
